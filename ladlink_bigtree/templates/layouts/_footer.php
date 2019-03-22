f461bd50edfd4419a8bb87ffcb7f058a07796138
Nathan Donkor <gerrard8donkor@gmail.com>
2019-03-22 17:15:13 +0000

    changr

--- ladlink_bigtree/templates/layouts/_footer.php
+++ ladlink_bigtree/templates/layouts/_footer.php
@@ -2,7 +2,7 @@
     <section class=" fill-burgundy" style="height: auto; min-height: 30vh;">
 			<div class="footer-logo-container section-width">
 				<div class="footer-logo-div">
-					<a href="http://ladlinkschool.com"><img class="footer-logo-white" src="images/ladlink-logo.png" alt="LADLINK LOGO"/></a>
+					<a href="<?=WWW_ROOT?>"><img class="footer-logo-white" src="<?=WWW_ROOT?>images/ladlink-logo.png" alt="LADLINK LOGO"/></a>
 					<span class="footer-school-title">Ladlink School</span>
 				</div>
 				<div class="footer-menu">
@@ -15,12 +15,12 @@
 						</div>
 					</div>
 					<div class="footer-site-map">
-						<div class="footer-menu-items"><a class="footer-link" href="http://ladlinkschool.com/">Home</a></div>
-						<div class="footer-menu-items"><a class="footer-link" href="http://ladlinkschool.com/about.html">About</a></div>
-						<div class="footer-menu-items"><a class="footer-link"  href="http://ladlinkschool.com/admissions.html">Admissions</a></div>
-						<div class="footer-menu-items"><a class="footer-link"  href="http://ladlinkschool.com/academics.html">Programmes</a></div>
-						<div class="footer-menu-items"><a  class="footer-link" href="http://ladlinkschool.com/campus_life.html">Campus Life</a></div>
-						<div class="footer-menu-items" style="border: none;"><a class="footer-link"  href="http://ladlinkschool.com/contact">Contact</a></div>
+						<div class="footer-menu-items"><a class="footer-link" href="<?=WWW_ROOT?>">Home</a></div>
+						<div class="footer-menu-items"><a class="footer-link" href="<?=WWW_ROOT?>_about">About</a></div>
+						<div class="footer-menu-items"><a class="footer-link"  href="<?=WWW_ROOT?>_admissions">Admissions</a></div>
+						<div class="footer-menu-items"><a class="footer-link"  href="<?=WWW_ROOT?>_academics">Programmes</a></div>
+						<div class="footer-menu-items"><a  class="footer-link" href="<?=WWW_ROOT?>_campus_life">Campus Life</a></div>
+						<div class="footer-menu-items" style="border: none;"><a class="footer-link"  href="<?=WWW_ROOT?>_contact">Contact</a></div>
 					</div>
 					
 				</div>
@@ -33,5 +33,5 @@
 		
 	</body>
 	<!-- Master  JS -->
-	<script src="js/logic.js"></script>
+	<script src="<?=WWW_ROOT?>js/logic.js"></script>
 </html>
--- ladlink_bigtree/templates/layouts/_header.php
+++ ladlink_bigtree/templates/layouts/_header.php
@@ -3,14 +3,14 @@
 
     <head>
         <title>Ladlink School</title>
-        <link rel="icon" href="images/ladlink-favicon.ico">
+        <link rel="icon" href="<?=WWW_ROOT?>images/ladlink-favicon.ico">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">        
         <!-- UIkit CSS -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.26/css/uikit.min.css" />
 		
 		<!-- Custom CSS -->
-        <link rel="stylesheet" href="css/custom.css" />
+        <link rel="stylesheet" href="<?=WWW_ROOT?>css/custom.css" />
         
 		<!-- UIkit JS -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.26/js/uikit.min.js"></script>
@@ -24,18 +24,18 @@
 		<!--HEADER-->
 		<header>
 			<div class="logo-div">
-				<a href="http://ladlinkschool.com"><img class="logo-white" src="images/ladlink-logo.png" alt="LADLINK LOGO"/></a>
+				<a href="<?=WWW_ROOT?>"><img class="logo-white" src="<?=WWW_ROOT?>images/ladlink-logo.png" alt="LADLINK LOGO"/></a>
 			</div>
 			<div class="header-upper">
 				<nav class="clear-menu">
 					<ul>
-						<li  class="uk-active"><a class="highlight" href="http://ladlinkschool.com/">Home</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/about.html">About</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/admissions.html">Admissions</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/academics.html">Programmes</a></li>
-<!--							<li><a class="highlight" href="http://ladlinkschool.com/campus_life.html">Campus Life</a></li>-->
-							<li><a class="highlight" href="http://ladlinkschool.com/contact.html">Contact</a></li>
-							<li><a  class="intro-section-button uk-button fill-burgundy splash-white" href="http://ladlinkschool.com/admissions.html">Apply Now</a></li>
+						<li  class="uk-active"><a class="highlight" href="<?=WWW_ROOT?>">Home</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_about">About</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_admissions">Admissions</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_academics">Programmes</a></li>
+<!--							<li><a class="highlight" href="<?=WWW_ROOT?>campus_life.html">Campus Life</a></li>-->
+							<li><a class="highlight" href="<?=WWW_ROOT?>_contact">Contact</a></li>
+							<li><a  class="intro-section-button uk-button fill-burgundy splash-white" href="<?=WWW_ROOT?>_admissions">Apply Now</a></li>
 							
 					</ul>
 
@@ -48,14 +48,14 @@
 			</div>
 				<nav class="mobile-menu" >
 					<ul>
-						<li class="uk-active"><a class="highlight" href="http://ladlinkschool.com/">Home</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/about.html">About</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/admissions.html">Admissions</a></li>
-							<li><a class="highlight" href="http://ladlinkschool.com/academics.html">Programmes</a></li>
-<!--							<li><a class="highlight" href="http://ladlinkschool.com/campus_life.html">Campus Life</a></li>-->
+						<li class="uk-active"><a class="highlight" href="<?=WWW_ROOT?>">Home</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_about">About</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_admissions">Admissions</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_academics">Programmes</a></li>
+<!--							<li><a class="highlight" href="<?=WWW_ROOT?>campus_life.html">Campus Life</a></li>-->
 
-							<li><a class="highlight" href="http://ladlinkschool.com/contact.html">Contact</a></li>
-							<li><a class="uk-button uk-button-default mobile-menu-button fill-burgundy">Apply Now</a></li>
+							<li><a class="highlight" href="<?=WWW_ROOT?>_contact">Contact</a></li>
+							<li><a class="uk-button uk-button-default mobile-menu-button fill-burgundy" href="<?=WWW_ROOT?>_admissions">Apply Now</a></li>
 					</ul>
 				</nav>
 			</div>
